43d21d971ffc6bda64641ad72de0d9214d2c9ac0
happyprogfrog <happyprogfrog@gmail.com>
2024-07-22 01:22:46 +0900

    docs: shop-dashboard에 Swagger 설정
    - http://localhost:8080/swagger-ui/index.html

---
+++ shop-dashboard/src/main/java/com/musinsa/shop/dashboard/config/SwaggerConfig.java
@@ -0,0 +1,12 @@
+package com.musinsa.shop.dashboard.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(title = "API 명세서",
+        description = "shop-dashboard API 명세서",
+        version = "1.0")
+)
+public class SwaggerConfig {
+}
